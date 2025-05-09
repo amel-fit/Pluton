ce58859a51214930e3399bbc371c0aa8cc48233c
amel-fit <husikaamel290@gmail.com>
2025-05-09 02:03:01 +0200

    Movement and dash revamp

--- Assets/Scripts/Management/InputManager.cs
+++ Assets/Scripts/Management/InputManager.cs
@@ -5,15 +5,18 @@ namespace Management
 {
     public class InputManager : MonoBehaviour
     {
-        public Action<float, float, bool> MovementInputReceived;
-        //public Action<bool> DashInputReceived; 
+        public Action<float, float> MovementInputReceived;
+        public Action<bool> DashInputReceived; 
 
         public void FixedUpdate()
         {
-            MovementInputReceived?.Invoke(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), Input.GetKeyDown(KeyCode.LeftShift));
-            //DashInputReceived?.Invoke(Input.GetKeyDown(KeyCode.LeftShift));
+            MovementInputReceived?.Invoke(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         }
 
-
+        public void Update()
+        {
+            //Has to be in update because FixedUpdate doesn't pick up on KeyDown consistently 
+            DashInputReceived?.Invoke(Input.GetKeyDown(KeyCode.LeftShift));
+        }
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections;
 using Management;
 using UnityEngine;
 
@@ -10,33 +10,64 @@ namespace PlayerController.Player
         private InputManager Movement;
 
         [SerializeField] private float speed;
-        private Vector3 direction = Vector3.zero;
+        public Vector3 direction = Vector3.zero;
         
         [Header("Dashing")]
-        [SerializeField] private int dashSpeed;
+        
+        [SerializeField] private float dashSpeed = 3;
+        [SerializeField] private float  dashTime = 0.25f;
+        [SerializeField] private float  dashCooldown = 0.5f;
+        
+        private bool _isDashing = false;
+        private bool _canDash = true;
         
         
         
         [SerializeField]
-        private Rigidbody playerRigidBody;
+        private CharacterController controller;
         
         
         private void Start()
         {
             Movement.MovementInputReceived += MovementInputReceived;
-            //Movement.DashInputReceived += DashInputReceived;
-            //playerRigidBody.rotation = Quaternion.Euler(0,-90,0);
+            Movement.DashInputReceived += DashInputReceived;
         }
 
-        
-
-        private void MovementInputReceived(float horizontal, float vertical, bool doDash)
+        private void DashInputReceived(bool doDash)
         {
-            MovePlayer(horizontal, vertical, doDash);
+            if (doDash && _canDash)
+                StartCoroutine(Dash());
+        }
 
+        private IEnumerator Dash()
+        {
+            _canDash = false;
+            
+            var startTime = Time.time;
+            _isDashing = true;
+            while (Time.time < startTime + dashTime)
+            {
+                //do the dash, dash speed compounds on regular speed so it doesn't have to be bigger than moveSpeed
+                controller.Move(direction * (dashSpeed * Time.fixedDeltaTime));
+                yield return null;
+            }
+            _isDashing = false;
+            
+            //Activate cooldown
+            var cooldown = new WaitForSeconds(dashCooldown);
+            yield return cooldown;
+            _canDash = true;
+        }
+        
+        private void MovementInputReceived(float horizontal, float vertical)
+        {
+            direction = new Vector3(horizontal, 0, vertical).normalized;
+            
             RotatePlayer(horizontal, vertical);
+            if(!_isDashing)
+                MovePlayer(direction * (Time.fixedDeltaTime * speed));
 
-            direction = Vector3.zero;
+            
         }
 
         private void RotatePlayer(float horizontal, float vertical)
@@ -45,26 +76,23 @@ namespace PlayerController.Player
             if (horizontal != 0 && vertical != 0)
             {
                 //45 degrees
-                // Debug.Log($"H: {horizontal}");
-                // Debug.Log($"V: {vertical}");
-                playerRigidBody.MoveRotation(Quaternion.Euler(new Vector3(0,1,0) * (45 + ((vertical > 0)? 0: 90)) * horizontal));
+                transform.rotation = Quaternion.Euler(new Vector3(0,1,0) * ((45 + ((vertical > 0)? 0: 90)) * horizontal));
             }else if (horizontal != 0)
             {
                 //left or right
-                playerRigidBody.MoveRotation(Quaternion.Euler(Vector3.up * 90 * horizontal));
+                transform.rotation = (Quaternion.Euler(Vector3.up * (90 * horizontal)));
             }else if (vertical != 0)
             {
                 //up or down
-                playerRigidBody.MoveRotation(Quaternion.Euler(Vector3.up * 180 * ((vertical > 0) ? 0 : 1) ));
+                transform.rotation = (Quaternion.Euler(Vector3.up * (180 * ((vertical > 0) ? 0 : 1)) ));
             }
         }
 
-        private void MovePlayer(float horizontal, float vertical, bool doDash)
+        private void MovePlayer(Vector3 directionAndSpeed)
         {
             //Debug.Log(horizontal + " " + vertical);
-            direction = new Vector3(horizontal, 0, vertical) * Time.fixedDeltaTime * (speed);
-            
-            playerRigidBody.MovePosition(new Vector3(this.playerRigidBody.position.x + direction.x, this.playerRigidBody.position.y, this.playerRigidBody.position.z + direction.z));
+            //Debug.Log($"{direction.x} {direction.y} {direction.z}");
+            controller.Move(directionAndSpeed);
         }
     }
 }
