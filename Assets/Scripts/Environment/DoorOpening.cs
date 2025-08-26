f27e350a784e972d637a04251691a53c766f40f2
vedad <vedolino2004@gmail.com>
2025-08-26 23:44:56 +0200

    to end credits

--- Assets/Scripts/Environment/DoorOpening.cs
+++ Assets/Scripts/Environment/DoorOpening.cs
@@ -1,15 +1,31 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DoorOpening : MonoBehaviour
 {
     [SerializeField] private Transform Enemies;
+    [SerializeField] private Transform EnemiesLast;
     [SerializeField] private float angle;
     void Update()
     {
         if (Enemies.childCount == 0)
         {
-            var opened = Quaternion.Euler(0, angle, 0);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, opened, 40f * Time.deltaTime);
+            OpenDoors();
         }
+        if (EnemiesLast.childCount == 0)
+        {
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void OpenDoors()
+    {
+        var opened = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, opened, 40f * Time.deltaTime);
     }
 }
