30dad9d2176c196606b0c46c178d94c579023b59
Amel <98781312+amel-fit@users.noreply.github.com>
2025-08-26 02:15:00 +0200

    Improve endscene
    add credits
    add fade-in effect

---
+++ Assets/Scripts/Management/FadeInTextManager.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Management
+{
+    public class FadeInTextManager : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text endText; 
+        [SerializeField]
+        private TMP_Text amelText;
+        [SerializeField]
+        private TMP_Text vedoText;
+
+
+        private float _fadeTime = 5.0f;
+        private float _startAlpha = 0;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+        private float _fadePerSecond;
+        void Start()
+        {
+            _fadePerSecond = 1 / _fadeTime;
+            StartCoroutine(FadeInEndText());
+        }
+
+
+        private IEnumerator Fade(TMP_Text textToFadeIn)
+        {
+            float fadeTimeLeft = _fadeTime;
+            float currentAlpha = _startAlpha;
+            while (fadeTimeLeft > 0)
+            {
+                fadeTimeLeft -= Time.deltaTime;
+                currentAlpha += _fadePerSecond * Time.deltaTime;
+
+                //Debug.Log(currentAlpha);
+                textToFadeIn.color = new Color(textToFadeIn.color.r, textToFadeIn.color.g, textToFadeIn.color.b,
+                    currentAlpha);
+                yield return null;
+            }
+
+            
+            yield return null;
+        }
+
+        
+        private IEnumerator FadeInEndText()
+        {
+            yield return new WaitForSeconds(3);
+            StartCoroutine(Fade(endText));
+            yield return new WaitUntil(() => endText.color.a > 0.9f);
+            yield return new WaitForSeconds(2.0f);
+            StartCoroutine(FadeInCredits());
+            yield return null;
+        }
+
+        private IEnumerator FadeInCredits()
+        {
+            StartCoroutine(Fade(amelText));
+            StartCoroutine(Fade(vedoText));
+            yield return null;
+        }
+
+        
+    }
+}
